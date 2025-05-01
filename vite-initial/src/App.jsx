180b9f57261afbb554feaab08558779307ae6841
Alexander Mamontov <alexander2555@yandex.ru>
2025-05-01 14:18:28 +0200

    Initial

---
+++ vite-initial/src/App.jsx
@@ -0,0 +1,23 @@
+import Logo from './assets/react.svg'
+import './App.css'
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={Logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
