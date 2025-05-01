5b28295f1cab6f94cac01872d99e43db6cec0af1
Alexander Mamontov <alexander2555@yandex.ru>
2025-05-01 20:54:46 +0200

    fix(App): Компонент переписан с JSX на JS
    Использована функция createElement вместо JSX-синтаксиса

---
+++ vite-initial/src/App.js
@@ -0,0 +1,33 @@
+import { createElement } from 'react'
+import Logo from './assets/react.svg'
+import './App.css'
+
+export const App = () => {
+  return createElement(
+    'div',
+    { className: 'App' },
+    createElement(
+      'header',
+      { className: 'App-header' },
+      createElement('img', { src: Logo, className: 'App-logo', alt: 'logo' }),
+      createElement(
+        'p',
+        null,
+        'Edit ',
+        createElement('code', null, 'src/App.js'),
+        ' and save to reload.',
+      ),
+      createElement(
+        'a',
+        {
+          className: 'App-link',
+          href: 'https://reactjs.org',
+          target: '_blank',
+          rel: 'noopener noreferrer',
+        },
+        'Learn React',
+      ),
+      createElement('span', null, new Date().getFullYear()),
+    ),
+  )
+}
