eb5fcbd038cb13a92287a8c60a417b7a4f3790e6
Alexander Mamontov <alexander2555@yandex.ru>
2025-05-05 12:39:56 +0200

    feat(app): Функционал калькулятора

--- vite-initial/src/App.jsx
+++ vite-initial/src/App.jsx
@@ -1,12 +1,97 @@
 import styles from './App.module.css'
-import { MyComponent } from './MyComponent'
+import { useState } from 'react'
 
 export const App = () => {
+  const add = (a, b) => a + b
+  const sub = (a, b) => a - b
+  const initialState = {
+    inputValue: 0,
+    stackValue: 0,
+    operation: add,
+  }
+
+  const [inputState, setInputState] = useState({ ...initialState })
+
+  const btnValues = []
+  for (let i = 0; i < 10; i++) {
+    btnValues.push(i)
+  }
+
   return (
     <div className={styles.app}>
-      <header className={styles.appHeader}>
-        <MyComponent />
-      </header>
+      <input type='text' value={String(inputState.inputValue)} readOnly />
+      {/* Кнопки для цифр 0-9 */}
+      {btnValues.map(btnValue => (
+        <button
+          key={btnValue}
+          value={btnValue}
+          onClick={() => {
+            console.log('inputState', inputState)
+            setInputState({
+              ...inputState,
+              inputValue: inputState.inputValue
+                ? inputState.inputValue * 10 + btnValue
+                : btnValue,
+            })
+          }}
+        >
+          {btnValue}
+        </button>
+      ))}
+      {/* Кнопки для операций */}
+      {/* Сброс */}
+      <button
+        onClick={() => {
+          setInputState({ ...initialState })
+        }}
+      >
+        C
+      </button>
+      {/* Сложение */}
+      <button
+        onClick={() => {
+          const a = inputState.stackValue
+          const b = inputState.inputValue
+          setInputState({
+            inputValue: 0,
+            stackValue: inputState.operation ? inputState.operation(a, b) : b,
+            operation: add,
+          })
+        }}
+      >
+        +
+      </button>
+      {/* Вычитание */}
+      <button
+        onClick={() => {
+          const a = inputState.stackValue
+          const b = inputState.inputValue
+          setInputState({
+            inputValue: 0,
+            stackValue: inputState.operation ? inputState.operation(a, b) : b,
+            operation: sub,
+          })
+        }}
+      >
+        -
+      </button>
+      {/* Результат */}
+      <button
+        disabled={!inputState.operation}
+        onClick={() => {
+          const result = inputState.operation
+            ? inputState.operation(inputState.stackValue, inputState.inputValue)
+            : inputState.inputValue
+          setInputState({
+            // ...inputState,
+            inputValue: result,
+            stackValue: result,
+            operation: null,
+          })
+        }}
+      >
+        =
+      </button>
     </div>
   )
 }
