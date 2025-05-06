3dc0ba1489150a63eff44c4fd17cfc21546c0a10
Alexander Mamontov <alexander2555@yandex.ru>
2025-05-06 12:17:41 +0200

    chore: Вывод значения на экран

--- react-components/src/App.jsx
+++ react-components/src/App.jsx
@@ -8,7 +8,8 @@ export const App = () => {
 
   const onInputButtonClick = () => {
     const promptValue = prompt('Введите значение')
-    console.log(promptValue)
+    setValue(promptValue)
+    // console.log(promptValue)
   }
 
   return (
@@ -16,7 +17,7 @@ export const App = () => {
       <h1 className={styles['page-heading']}>Ввод значения</h1>
       <p className={styles['no-margin-text']}>
         Текущее значение <code>value</code>: "
-        <output className={styles['current-value']}></output>"
+        <output className={styles['current-value']}>{value}</output>"
       </p>
       <div className={styles.error}>
         Введенное значение должно содержать минимум 3 символа
