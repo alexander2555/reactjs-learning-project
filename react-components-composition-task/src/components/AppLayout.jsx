dd7971724e32f6e5542d36ceaddd8117651f13b9
Alexander Mamontov <alexander2555@yandex.ru>
2025-05-21 20:19:06 +0200

    feat: Кнопка перезапуска игры

--- react-components-composition-task/src/components/AppLayout.jsx
+++ react-components-composition-task/src/components/AppLayout.jsx
@@ -3,11 +3,14 @@ import styles from './App.module.css'
 import { Info } from './Info/Info'
 import { Field } from './Field/Field'
 
-export const AppLayout = ({ gameState, setGameState }) => {
+export const AppLayout = ({ gameState, setGameState, restartGame }) => {
   return (
     <div className={styles.app}>
       <Info {...gameState} />
       <Field {...gameState} setGameState={setGameState} />
+      <button className={styles['restart-btn']} onClick={restartGame}>
+        Начать заново
+      </button>
     </div>
   )
 }
