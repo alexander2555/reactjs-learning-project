3b4fc9e76631619b81a9e690d27b047ec9fb62a8
Alexander Mamontov <alexander2555@yandex.ru>
2025-05-29 14:39:05 +0200

    feat: Валидация

--- react-forms/src/App.jsx
+++ react-forms/src/App.jsx
@@ -1,18 +1,83 @@
 import { useStore } from './useStore'
 import styles from './App.module.css'
+import { useState, useRef } from 'react'
 
 const initialState = {
   email: '',
   pass: '',
   passRepeat: '',
 }
+const initialValidationState = {
+  ok: false,
+  valErrors: '',
+}
+const validateForm = ({ email, pass, passRepeat }) => {
+  const errors = []
+
+  console.log(email, pass, passRepeat)
+
+  const emptyFields = !(email && pass && passRepeat)
+  if (emptyFields) {
+    errors.push('Все поля формы должны быть заполнены!')
+  } else {
+    const emailRegex = /^\w+@[a-zA-Z]+\.[a-z]{2,}$/
+    const passRegex = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])/
+
+    if (email && !emailRegex.test(email)) {
+      errors.push('Адрес email некорректен!')
+    }
+
+    if (pass) {
+      if (passRepeat && pass !== passRepeat) {
+        errors.push('Пароли не совпадают!')
+      }
+
+      if (pass.length < 4) {
+        errors.push('Пароль короче 4-х символов')
+      }
+
+      if (!passRegex.test(pass)) {
+        errors.push(
+          'Пароль должен содержать строчные и прописные буквы, цифры и символы @$!%*?&',
+        )
+      }
+    }
+  }
+
+  return { valOk: !errors.length, valErrors: errors.join('\n') }
+}
 
 export const App = () => {
   const { getState, updateState } = useStore(initialState)
+  const [valState, setValError] = useState(initialValidationState)
 
   const { email, pass, passRepeat } = getState()
 
-  const onChangeField = ({ target }) => updateState({ [target.name]: target.value })
+  const submitBtnRef = useRef(null)
+
+  const onChangeField = ({ target }) => {
+    updateState({ [target.name]: target.value })
+    const { valOk } = validateForm({
+      ...getState(),
+      [target.name]: target.value,
+    })
+    if (valOk) {
+      setValError(initialValidationState)
+      submitBtnRef.current.focus()
+    }
+  }
+  const onBlurField = ({ target }) => {
+    // const passfields = {
+    //   pass: getState().pass,
+    //   passRepeat: getState().passRepeat,
+    // }
+    setValError(
+      validateForm({
+        ...getState(),
+        [target.name]: target.value,
+      }),
+    )
+  }
 
   const onFormSubmit = e => {
     e.preventDefault()
@@ -21,12 +86,14 @@ export const App = () => {
 
   return (
     <div className={styles.app}>
-      <form onSubmit={onFormSubmit}>
+      <form onSubmit={onFormSubmit} noValidate>
+        {!valState.valOk && <p className={styles['error-label']}>{valState.valErrors}</p>}
         <input
           name='email'
           type='email'
           value={email}
           onChange={onChangeField}
+          onBlur={onBlurField}
           placeholder='Email'
         />
         <input
@@ -34,6 +101,7 @@ export const App = () => {
           type='password'
           value={pass}
           onChange={onChangeField}
+          onBlur={onBlurField}
           placeholder='Пароль'
         />
         <input
@@ -41,9 +109,18 @@ export const App = () => {
           type='password'
           value={passRepeat}
           onChange={onChangeField}
+          onBlur={onBlurField}
           placeholder='Пароль ещё раз'
         />
-        <button type='submit'>Зарегистрироваться</button>
+        <button
+          ref={submitBtnRef}
+          type='submit'
+          aria-disabled={!valState.valOk}
+          // disabled={valError}
+          className={`${styles['btn-submit']}${!valState.valOk ? ' ' + styles.disabled : ''}`}
+        >
+          Зарегистрироваться
+        </button>
       </form>
     </div>
   )
