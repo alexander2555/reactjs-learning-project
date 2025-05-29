8ca7cfbaf31e75659791465e51fdf94029c43177
Alexander Mamontov <alexander2555@yandex.ru>
2025-05-29 14:39:05 +0200

    feat: Валидация

--- react-forms/src/useStore.js
+++ react-forms/src/useStore.js
@@ -1,7 +1,15 @@
 import { useState } from 'react'
 
+/**
+ * Управление состоянием
+ *
+ * @param {Object} initialState - начальное состояние
+ *
+ * @returns {Object} - Объект с методами управления состоянием
+ */
 export const useStore = initialState => {
   const [state, setState] = useState(initialState)
+
   return {
     getState: () => state,
     updateState: newState =>
