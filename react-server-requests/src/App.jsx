4b6f8efd50561e0e7a6c7348da85f0a19cdf777e
Alexander Mamontov <alexander2555@yandex.ru>
2025-06-02 22:18:08 +0200

    chore: Backend mock

--- react-server-requests/src/App.jsx
+++ react-server-requests/src/App.jsx
@@ -3,17 +3,22 @@ import styles from './App.module.css'
 
 export const App = () => {
   const [products, setProducts] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
-    fetch('https://mocki.io/v1/399c66c5-e6e9-4c0c-96a6-28ed14af5d8a')
+    setIsLoading(true)
+
+    fetch('http://localhost:3003/products/?id=001')
       .then(data => data.json())
       .then(p => {
         setProducts(p)
       })
+      .finally(() => setIsLoading(false))
   }, [])
 
   return (
     <div className={styles.app}>
+      {isLoading && <div className={styles.loader}></div>}
       {products.map(({ id, name, price }) => (
         <div key={id}>
           {name}: {price} р.
