84f21c0ff963a772c989b2f710410a326dfa4899
Alexander Mamontov <alexander2555@yandex.ru>
2025-06-14 10:34:18 +0200

    fix: React Context - useContext to manage data
    Использование use(Context) вместо проброса props

--- react-context/src/components/header/components/current-user/CurrentUser.jsx
+++ react-context/src/components/header/components/current-user/CurrentUser.jsx
@@ -1,7 +1,11 @@
-export const CurrentUser = ({ currentUser }) => {
+import { use } from 'react'
+import { AppContext } from '../../../../context'
+
+export const CurrentUser = () => {
+  const { name } = use(AppContext)
   return (
     <div>
-      <div>Текущий пользователь: {currentUser}</div>
+      <div>Текущий пользователь: {name}</div>
     </div>
   )
 }
