f5bd1cbb94f8a7f5b0123cf7dbff7a4569df1267
Alexander Mamontov <alexander2555@yandex.ru>
2025-06-14 13:22:21 +0200

    fix: React Context - state management with reducer

--- react-context/src/components/header/components/current-user/CurrentUser.jsx
+++ react-context/src/components/header/components/current-user/CurrentUser.jsx
@@ -2,10 +2,22 @@ import { use } from 'react'
 import { AppContext } from '../../../../context'
 
 export const CurrentUser = () => {
-  const { name } = use(AppContext)
+  const { userData, dispatch } = use(AppContext)
+  const { name } = userData
+
   return (
     <div>
       <div>Текущий пользователь: {name}</div>
+      <button
+        onClick={() =>
+          dispatch({
+            type: 'SET_USER_DATA',
+            payload: { ...userData, name: 'новый' },
+          })
+        }
+      >
+        Обновить
+      </button>
     </div>
   )
 }
